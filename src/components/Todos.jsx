bf0582bc4b1e55f4ee3521a79d764a1f52c7d291
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-12 19:35:43 +0330

    add todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodos } from "../redux/reducer";
 
 const Todos = () => {
+  const todos = useSelector((state) => state);
+
+  const dispatch = useDispatch();
   const [todo, setTodo] = useState("");
 
   const handleChange = (e) => {
@@ -14,7 +19,26 @@ const Todos = () => {
         onChange={(e) => handleChange(e)}
         className="todo-input"
       />
-      <button className="add-btn">Add</button>
+      <button
+        className="add-btn"
+        onClick={() =>
+          dispatch(
+            addTodos({
+              id: Math.floor(Math.random() * 1000),
+              item: todo,
+              completed: false
+            })
+          )
+        }
+      >
+        Add
+      </button>
+      <br />
+      <ul>
+        {todos.map(({ id, item }) => (
+          <li key={id}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
