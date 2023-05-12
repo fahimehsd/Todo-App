1e9bcc8996a820b496837501cd1ba9bb9e45cfbd
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-12 19:18:21 +0330

    add todo input added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Todos from "./components/Todos";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 };
 
 export default App;
